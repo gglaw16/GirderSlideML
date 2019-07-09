645555d220564bfc83ab6440d04fa4983015ded8
Gwenda Law <gwenda.law@gmail.com>
2019-07-09 15:50:47 -0400

    getting an image from a annotation box

--- get_annotation_location.py
+++ get_annotation_location.py
@@ -7,16 +7,30 @@ Created on Tue Jul  9 14:19:26 2019
 """
 
 import girder as g
+import math
+import matplotlib.pyplot as plt
+import cv2
 
+level = 2
 
 gc = g.get_gc()
-image_id = '5d24bdd370aaa9038e6d1cb4'
-name = 'ROI'
+item_id = '5d24bdd370aaa9038e6d1cb4'
+name = 'gwenda.law'
 
-annotation_id = g.get_annotation_id_from_name(image_id, name, gc)
+annotation_id = g.get_annotation_id_from_name(item_id, name, gc)
 
 center, width, height = g.get_annotation_loc_from_id(annotation_id, gc)
 
-print(center)
-print(width)
-print(height)
+spacing = math.pow(2, level)
+
+x = center[0]
+y = center[1]
+w = width/spacing
+h = height/spacing
+
+image = g.get_image_cutout(gc, item_id, (x,y), w, h, scale=1.0/spacing, cache='cache')
+
+im_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+
+plt.imshow(im_rgb)
